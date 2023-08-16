9d019b2cc03956682932d64fb82d45a2333a2536
Felipe Santos <felipesantos_fcs98@hotmail.com>
2023-08-16 03:18:15 -0300

    first commit

---
+++ src/components/Botao.vue
@@ -0,0 +1,37 @@
+<template>
+    <button class="button" @click="clicado" :disabled="desabilitado">
+        <span class="icon">
+            <i :class="icone"></i>
+        </span>
+        <span>{{ nomeBotao }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'Botao',
+    emits: ['clicado'],
+    props: {
+        nomeBotao: {
+            type: String,
+            required: true
+        },
+        icone: {
+            type: String,
+            required: true
+        },
+        desabilitado: {
+            type: Boolean
+        }
+    },
+    methods: {
+        clicado() : void{
+            this.$emit('clicado')
+        }
+    }
+})
+</script>
+
+<style scoped></style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,35 @@
+<template>
+    <Box>
+        <div class="columns">
+            <div class="column is-7">
+                {{tarefa.descricao || 'Tarefa sem Descrição'}}
+            </div>
+            <div class="column">
+                <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+            </div>
+        </div>
+    </Box>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropsType } from 'vue';
+import Cronometro from './Cronometro.vue';
+import ITarefa from '../interfaces/ITarefa';
+import Box from './Box.vue'
+
+export default defineComponent({
+    name: 'Tarefa',
+    components: {
+        Cronometro,
+        Box
+    },
+    props:{
+        tarefa:{
+            type: Object as PropsType<ITarefa>,
+            required: true
+        } 
+    }
+})
+</script>
+
+
